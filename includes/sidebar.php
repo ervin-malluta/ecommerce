70893af5b6e1517d4e6db8424088e5f0a6834ee7
ervin_malluta <ervinmalluta2@gmail.com>
2022-11-05 17:01:16 +0300

    sidebar update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -22,24 +22,44 @@
 	</div>
 </div>
 
-<div class="row">
-	<div class="box box-solid">
-	  	<div class="box-header with-border">
-	    	<h3 class="box-title"><b>Become a Subscriber</b></h3>
-	  	</div>
-	  	<div class="box-body">
-	    	<p>Get free updates on the latest products and discounts, straight to your inbox.</p>
-	    	<form method="POST" action="">
-		    	<div class="input-group">
-	                <input type="text" class="form-control">
-	                <span class="input-group-btn">
-	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
-	                </span>
-	            </div>
-		    </form>
-	  	</div>
-	</div>
-</div>
+<?php
+		       			$month = date('m');
+		       			$conn = $pdo->open();
+
+		       			try{
+		       			 	$inc = 3;	
+						    $stmt = $conn->prepare("SELECT * from products Order By name DESC Limit 2");
+						    $stmt->execute();
+							echo "<div class='box-header with-border'>
+									<h3 class='box-title'><b>Today's Suggestions</b></h3>
+						  		</div>";
+						    foreach ($stmt as $row) {
+						    	$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
+						    	$inc = ($inc == 3) ? 1 : $inc + 1;
+	       						if($inc == 1) echo "<div class='row'>";
+	       						echo "
+	       								<div class='box box-solid'>
+		       								<div class='box-body prod-body'>
+		       									<img src='".$image."' width='100%' height='230px' class='thumbnail'>
+		       									<h5><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
+		       								</div>
+		       								<div class='box-footer'>
+		       									<b>&#36; ".number_format($row['price'], 2)."</b>
+		       								</div>
+	       								</div>
+	       						";
+	       						if($inc == 3) echo "</div>";
+						    }
+						    if($inc == 1) echo "<div class='col-sm-4'></div><div class='col-sm-4'></div></div>"; 
+							if($inc == 2) echo "<div class='col-sm-4'></div></div>";
+						}
+						catch(PDOException $e){
+							echo "There is some problem in connection: " . $e->getMessage();
+						}
+
+						$pdo->close();
+
+		       		?> 
 
 <div class="row">
 	<div class='box box-solid'>
